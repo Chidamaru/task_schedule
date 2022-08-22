8c1f96adc1592fc67f764d51c6bc728a70df0acb
下高原汐理 <shimo.shiori@kakougenshiorinoMacBook-Air.local>
2022-08-22 17:05:47 +0900

    initial commit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,7 +8,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(params.require(:task).permit(:name, :start, :finish))
+    @task = Task.new(params.require(:task).permit(:name, :start, :finish, :discription))
     if @task.save
       flash[:notice] = "スケジュールを新規登録しました"
       redirect_to :tasks
@@ -22,11 +22,23 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
   def update
+    @task = Task.find(params[:id])
+    if @task.update(params.require(:task).permit(:name, :start, :finish, :discription))
+      flash[:notice] = "#{@task.id}のスケジュールを更新しました"
+      redirect_to :tasks
+    else
+      render "edit"
+    end
   end
 
   def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    flash[:notice] = "#{@task.id}のスケジュールを削除しました"
+    redirect_to :tasks
   end
 end
